8039f9eb792d0abc42fa87b975f54d3d09c09409
Anderson Salles <agsalles@gmail.com>
2018-08-31 20:18:49 -0300

    Projeto criado

---
+++ src/main/java/br/com/devapps/apipedido/ApipedidoApplication.java
@@ -0,0 +1,12 @@
+package br.com.devapps.apipedido;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApipedidoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApipedidoApplication.class, args);
+	}
+}
