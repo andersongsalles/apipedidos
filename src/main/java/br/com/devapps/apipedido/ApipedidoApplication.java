2dd37e8260c94a6e0ad652573a2066b6ec0123dd
Anderson Salles <agsalles@gmail.com>
2018-09-02 18:18:25 -0300

    Produto e associacao muitos para muitos

--- src/main/java/br/com/devapps/apipedido/ApipedidoApplication.java
+++ src/main/java/br/com/devapps/apipedido/ApipedidoApplication.java
@@ -8,13 +8,17 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import br.com.devapps.apipedido.domain.Categoria;
+import br.com.devapps.apipedido.domain.Produto;
 import br.com.devapps.apipedido.repositories.CategoriaRepository;
+import br.com.devapps.apipedido.repositories.ProdutoRepository;
 
 @SpringBootApplication
 public class ApipedidoApplication implements CommandLineRunner{
 	
 	@Autowired
-	private CategoriaRepository categoriaRepository;
+	private CategoriaRepository categoriaRepository;	
+	@Autowired
+	private ProdutoRepository produtoRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApipedidoApplication.class, args);
@@ -26,11 +30,44 @@ public class ApipedidoApplication implements CommandLineRunner{
 		Categoria cat1 = new Categoria(null, "Informática");
 		Categoria cat2 = new Categoria(null, "Escritório");
 		
+		Produto p1 = new Produto(null, "Computador", 2000.00);
+		Produto p2 = new Produto(null, "Impressora", 800.00);
+		Produto p3 = new Produto(null, "Impressora", 80.00);
+		
+		cat1.getProdutos().addAll(Arrays.asList(p1, p2, p3));
+		cat2.getProdutos().addAll(Arrays.asList(p2));
+		
+		p1.getCategoria().addAll(Arrays.asList(cat1));
+		p2.getCategoria().addAll(Arrays.asList(cat1, cat2));
+		p3.getCategoria().addAll(Arrays.asList(cat1));
+		
 		categoriaRepository.save(Arrays.asList(cat1, cat2));
+		produtoRepository.save(Arrays.asList(p1,p2,p3));
 		
 		
 		
 		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+				
 		
 	}
 }
