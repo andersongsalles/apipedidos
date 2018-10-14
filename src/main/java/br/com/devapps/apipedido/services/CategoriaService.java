ae4d0e88b2f229fe0a84e923ff7295d48266b3e1
Anderson Salles <agsalles@gmail.com>
2018-10-14 17:26:08 -0300

    Deletando uma categoria com DELETE

--- src/main/java/br/com/devapps/apipedido/services/CategoriaService.java
+++ src/main/java/br/com/devapps/apipedido/services/CategoriaService.java
@@ -1,10 +1,12 @@
 package br.com.devapps.apipedido.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import br.com.devapps.apipedido.domain.Categoria;
 import br.com.devapps.apipedido.repositories.CategoriaRepository;
+import br.com.devapps.apipedido.services.exception.DataIntegrityException;
 import br.com.devapps.apipedido.services.exception.ObjectNotFoundException;
 
 @Service
@@ -31,5 +33,15 @@ public class CategoriaService {
 		find(obj.getId());
 		return repo.save(obj);
 	}
+
+	public void delete(Integer id) {
+		find(id);
+		try {
+			repo.delete(id);		
+		}catch(DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Não é possível excluir uma categoria que possui produtos");
+		}
+	}
+		
 
 }
