ac776fc111c97c4e3dc16e0b85fb5891a45a5c33
Anderson Salles <agsalles@gmail.com>
2018-10-26 20:49:36 -0300

    Listando todas categorias com DTO

--- src/main/java/br/com/devapps/apipedido/resources/CategoriaResource.java
+++ src/main/java/br/com/devapps/apipedido/resources/CategoriaResource.java
@@ -1,6 +1,9 @@
 package br.com.devapps.apipedido.resources;
 
 import java.net.URI;
+import java.util.List;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -12,6 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import br.com.devapps.apipedido.domain.Categoria;
+import br.com.devapps.apipedido.dto.CategoriaDTO;
 import br.com.devapps.apipedido.services.CategoriaService;
 
 @RestController
@@ -47,5 +51,12 @@ public class CategoriaResource {
 	public ResponseEntity<Void> delete(@PathVariable Integer id){
 		service.delete(id);
 		return ResponseEntity.noContent().build();
+	}
+	
+	@RequestMapping(method=RequestMethod.GET)
+	public ResponseEntity<List<CategoriaDTO>> findAll(){
+		List<Categoria> listCategoria = service.findAll();
+		List<CategoriaDTO> listCategoriaDto = listCategoria.stream().map(categoria -> new CategoriaDTO(categoria)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listCategoriaDto);
 	}
 }
